15388b8f2662e708a5a1e09540c8a9b4876787a1
aadennis <dennis.circ@gmail.com>
2016-09-21 23:09:07 +0100

    refactored file name content display

--- Utilities/ZipUtilities.ps1
+++ Utilities/ZipUtilities.ps1
@@ -24,14 +24,27 @@ function Get-NamesInZipFile {
         Write-Host "File [$zipFileLocation] not found. Exiting..." -BackgroundColor Yellow -ForegroundColor Red
         throw
     }
-
+    $entries = $null
     [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
     $entries = [IO.Compression.ZipFile]::OpenRead($zipFileLocation).Entries
     Write-Host "[$($entries.count)] File names found in [$zipFileLocation]:" -BackgroundColor Black -ForegroundColor White
-    $entries | % {"$_"}
-    $entries | % {"$_"} | clip
+  
+    Write-ArrayContentToScreen $entries "stuf and stuff" -numberedList
     Write-Host "File names found in [$zipFileLocation] are in your clipboard" -BackgroundColor Black -ForegroundColor White
 
+}
+
+function Write-ArrayContentToScreen {
+[CmdletBinding()]
+Param (
+    [Parameter(mandatory = $true)] $ArrayToDisplay,
+    [Parameter(mandatory = $true)] $Message,
+    [switch] $numberedList
+) 
+    foreach ($filename in  $ArrayToDisplay) {
+        $filename.FullName
+    }
+    $ArrayToDisplay | % {"$_"} | clip
 }
 
 #Get-NamesInZipFile "C:\scratch\CreateADBDC.ps1.zip"
