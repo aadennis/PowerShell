29e613334b29d968092313ca6d814be80e906b19
aadennis <dennis.circ@gmail.com>
2016-10-12 23:39:34 +0100

    added a test

--- Utilities/Tests/DateFormatUtility.Tests.ps1
+++ Utilities/Tests/DateFormatUtility.Tests.ps1
@@ -3,6 +3,7 @@
         convertFrom-yymmddhhmmssFormat "000000000020120808235959" | should be "08/08/2012"
         convertFrom-yymmddhhmmssFormat "000000000020011231225953" | should be "31/12/2001"
         convertFrom-yymmddhhmmssFormat "000000000010660228230022" | should be "28/02/1066"
+        convertFrom-yymmddhhmmssFormat "10660228230022" | should be "28/02/1066"
     }
 
     It "converts an incoming malformed date to 00/00/0000" {
