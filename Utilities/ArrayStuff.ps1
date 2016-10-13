aaf10d6c34d312e0e7214d6e7d451cea998f19b0
aadennis <dennis.circ@gmail.com>
2016-10-13 23:08:36 +0100

    testing array behaviour

---
+++ Utilities/ArrayStuff.ps1
@@ -0,0 +1,19 @@
+﻿Clear-Host
+$x = [System.Collections.ArrayList]@()
+$x.Add("Row number 1")
+$x.Add("Row number 2")
+$x.Add("Row number 3")
+$x.Add("Row number 4")
+$x.Add("Row number 5")
+$x.Add("Row number 6")
+"Count of x:"
+$x.Count
+
+$y = [array] $x
+[array]::Reverse([array]$y)
+"x cast to y as array and reversed:"
+$y
+
+"x reverse using cast to array silently fails:"
+[array]::Reverse([array]$x)
+$x
