ffdfe58119ebe5c59d5fbeff9ba4893b02d1432c
Dennis Wells <dennis.circ@gmail.com>
2016-10-16 11:13:57 +0100

    Capture return value from invoke-pester

--- Utilities/Tests/RunPester.ps1
+++ Utilities/Tests/RunPester.ps1
@@ -1,6 +1,26 @@
-﻿# Execute this from the folder [Tests]
+﻿#https://github.com/pester/Pester/wiki/Invoke-Pester
+# Execute this from the folder [Tests]
 
 . ..\DateFormatUtility\DateFormatUtility.ps1
 . ..\LogUtility\LogUtility.ps1
 
-Invoke-Pester -Verbose
+# Execute all the tests
+$result = Invoke-Pester -PassThru
+
+# Did any tests fail? If so, in a more rigorous scenario, the throw would be replaced
+# with some way to tell the build to fail
+$totalCount = $result.TotalCount
+$passedCount = $result.PassedCount
+$failedCount = $result.FailedCount
+$msg = "Total tests:[$totalCount]; Passed:[$passedCount]; Failed:[$failedCount]"
+if ($failedCount -gt -1) {
+    Write-Host $msg -f red -b White
+    Throw
+}
+Write-Host $msg -f Green -b Blue
+ 
+
+
+
+
+
