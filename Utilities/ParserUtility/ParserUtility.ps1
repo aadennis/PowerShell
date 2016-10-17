fad66f272892a397b20a56361adb2d692182defa
Dennis Wells <dennis.circ@gmail.com>
2016-10-17 23:41:05 +0100

    Start of ParserUtility

---
+++ Utilities/ParserUtility/ParserUtility.ps1
@@ -0,0 +1,10 @@
+﻿
+
+$p=[System.Management.Automation.Language.Parser]::ParseFile('C:\tempo\PowerShell\Utilities\LogUtility\LogUtility.ps1',[ref]$tokens,[ref]$parseerrors)
+# now review parsedResults, tokens and parseerrors
+$p.Extent
+# list the tokens
+#$tokens|ft -auto
+
+$parseerrors.Count
+
