8a192f93c0676aa961d5e7e9533d85b4f30e7188
aadennis <dennis.circ@gmail.com>
2016-11-15 20:56:34 +0000

    Fixed the multiline modifier

--- Utilities/RegexUtility/RegexShowcase.ps1
+++ Utilities/RegexUtility/RegexShowcase.ps1
@@ -69,14 +69,46 @@ $regexPattern = New-Object regex("[\+-].*")
 $candidateMatch = $regexPattern.IsMatch("x")
 $candidateMatch
 
-# cannot get this working - New-Object : Exception calling ".ctor" with "1" argument(s): "parsing "m)^cricket" - Too many )'s.
+#https://msdn.microsoft.com/en-us/library/system.text.regularexpressions.regex(v=vs.110).aspx
 
-$subPattern = "m`)"
-$regexPattern = New-Object regex($subPattern + "^cricket")
+# multiline mode modifier
+#https://msdn.microsoft.com/en-us/library/yd1hzczs(v=vs.110).aspx#Multiline
+
+$regexPattern = [regex] '(?m)^cricket$'
 $regexPattern
 
 $candidateMatch = $regexPattern.IsMatch("cricket")
 $candidateMatch
 
-$candidateMatch = $regexPattern.IsMatch("football`r`ncricket")
+# note that the input string still escapes using backtick not backslash...
+$candidateMatch = $regexPattern.IsMatch("cricket`r`ncricket")
+$candidateMatch
+
+"-----"
+# For all the records in the input, true if at least 1 line starts with cri, ends with cket, and has 0 or many spaces
+$regexPattern = [regex] '(?m)^cri(\s*)cket$'
+$regexPattern
+
+# true because there are 0 or many spaaces beween cri and cket...
+$candidateMatch = $regexPattern.IsMatch("cricket")
+$candidateMatch
+
+#true because rule above obeyed on both lines...
+$candidateMatch = $regexPattern.IsMatch("cric     ket`r`ncricket")
+$candidateMatch
+
+#true because on line 1, [cricc] has all the characters required by [cric]
+$candidateMatch = $regexPattern.IsMatch("cricc     ket`r`ncricket")
+$candidateMatch
+
+#true because on line 1, [crick] has all the characters required by [cric]
+$candidateMatch = $regexPattern.IsMatch("crick     ket`r`ncricket")
+$candidateMatch
+
+#false because neither line 1 nor line 1 match the pattern
+$candidateMatch = $regexPattern.IsMatch("crikk     ket`r`ncrikket")
+$candidateMatch
+
+#false because [cket] cannot be found...
+$candidateMatch = $regexPattern.IsMatch("cric     ket")
 $candidateMatch
