fba45f957ec487c6560ecbe0706f4e9063421ca7
aadennis <dennis.circ@gmail.com>
2016-11-16 20:46:43 +0000

    Added more tests
    See
    [https://msdn.microsoft.com/en-us/library/system.text.regularexpressions.regex(v=vs.110).aspx]

--- Utilities/RegexUtility/RegexShowcase.ps1
+++ Utilities/RegexUtility/RegexShowcase.ps1
@@ -2,11 +2,15 @@
 
 # multiline mode modifier
 #https://msdn.microsoft.com/en-us/library/yd1hzczs(v=vs.110).aspx#Multiline
+
+$Global:testId = 0
 Set-StrictMode -Version latest
 
 Add-Type -AssemblyName "System.Text.RegularExpressions"
 
 function Test-RegexMatch($pattern, $inputToMatch, $message, $expectedResult) {
+    $Global:testId++
+
     $regexPattern = $null
     $regexPattern = New-Object regex($pattern)
    
@@ -14,7 +18,7 @@ function Test-RegexMatch($pattern, $inputToMatch, $message, $expectedResult) {
 
     $actualBooleanResult = $($regexPattern.IsMatch($inputToMatch)).ToString().ToUpper()
 
-    Write-Host "pattern:[$pattern]input:[$inputToMatch][expected result:$expectedResult ($pattern) $message][actual result:$actualBooleanResult]"
+    Write-Host "Test [$Global:testId] pattern:[$pattern]input:[$inputToMatch][expected result:$expectedResult ($pattern) $message][actual result:$actualBooleanResult]"
     if ($actualBooleanResult -ne $expectedResult) {
         throw "*** Last test failed. exiting... ***"
     }
@@ -196,6 +200,138 @@ Test-RegexMatch $pattern $input $message $expectedResult
 $input = "cricketcricketcricketcricketcricket"
 $expectedResult = "TRUE"
 $message = "cricket must occur 1 or many times in the input, and does"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+
+#Match zero or one occurrence of either the positive sign or the negative sign.
+$pattern = "[\+-]?"
+
+$input = "+"
+$expectedResult = "TRUE"
+$message = "match found at position zero"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "++"
+$expectedResult = "TRUE"
+$message = "match found at position zero"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+
+$input = "-"
+$expectedResult = "TRUE"
+$message = "match found at position zero"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "x--"
+$expectedResult = "TRUE"
+$message = "no match found, so matches on zero matches"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-x-"
+$expectedResult = "TRUE"
+$message = "no match found, so matches on zero matches"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+#Match zero or one occurrence of the dollar sign.
+
+$pattern = "`\`$`?"
+
+$input = ""
+$expectedResult = "TRUE"
+$message = "no match found, so matches on zero matches"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+
+$input = "`$29"
+$expectedResult = "TRUE"
+$message = "dollar found - match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "`$`$29"
+$expectedResult = "TRUE"
+$message = "dollar found at position 0 - match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$pattern = "^[\+-]+`\`$"
+
+$input = "`$29"
+$expectedResult = "FALSE"
+$message = "+ symbol should have been before `$ - no match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29"
+$expectedResult = "TRUE"
+$message = "- symbol was before `$ - match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$pattern = "^[\+-]+`\`$\d{2}?"
+
+$input = "`$29"
+$expectedResult = "FALSE"
+$message = "+ symbol should have been before `$ - no match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29"
+$expectedResult = "TRUE"
+$message = "- symbol was before `$ - match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$"
+$expectedResult = "FALSE"
+$message = "input contained no digits - no match"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$483"
+$expectedResult = "TRUE"
+$message = "pattern asked for match on 2 decimal zero or one times. OK"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$pattern = "^[\+-]+`\`$\d*\.?"
+
+
+$input = "-`$29."
+$expectedResult = "TRUE"
+$message = "a found at least one dot after the digits"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29.."
+$expectedResult = "TRUE"
+$message = "b found at least one dot after the digits"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29.44"
+$expectedResult = "TRUE"
+$message = "c found at least one dot after the digits and don't care after that"
+Test-RegexMatch $pattern $input $message $expectedResult
+#---
+
+$pattern = "^[\+-]+`\`$\d*\.?\d{2}?"
+
+
+
+$input = "-`$29."
+$expectedResult = "TRUE"
+$message = "digits after the dot are optional - found none - OK"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29.."
+$expectedResult = "TRUE"
+$message = "digits after the dot are optional - found none - OK"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29.44"
+$expectedResult = "TRUE"
+$message = "digits after the dot are optional - found some - OK"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$29.444"
+$expectedResult = "TRUE"
+$message = "digits after the dot are optional - found the max 2 and ignored the rest - OK"
+Test-RegexMatch $pattern $input $message $expectedResult
+
+$input = "-`$483"
+$expectedResult = "TRUE"
+$message = "Both the dot and the digits after the dot are optional. OK"
 Test-RegexMatch $pattern $input $message $expectedResult
 
 return
