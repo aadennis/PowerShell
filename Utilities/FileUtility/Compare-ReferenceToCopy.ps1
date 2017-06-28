81a949de131ca62ac67a83c8a98006561e85ad7d
Dennis Wells <aadennis@users.noreply.github.com>
2017-06-28 22:50:08 +0100

    Delete Compare-ReferenceToCopy.ps1

--- Utilities/FileUtility/Compare-ReferenceToCopy.ps1
+++
@@ -1,9 +0,0 @@
-# Compare a reference folder and children, with a copy of that reference
-$refPath = "G:\VideosCollection\TheRest\MP4"
-$copyPath = "e:\set3"
-
-$refSet = Get-ChildItem -Recurse -path $refPath
-$copySet = Get-ChildItem -Recurse -path $copyPath
-
-Compare-Object -ReferenceObject $refSet -DifferenceObject $copySet
-
