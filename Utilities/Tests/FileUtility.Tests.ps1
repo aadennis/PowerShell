b1470d126da2ba3cc093f3f060d148aaddfa0111
Dennis Wells <dennis.circ@gmail.com>
2017-07-09 18:41:05 +0100

    Rephrased for Describing and Context

--- Utilities/Tests/FileUtility.Tests.ps1
+++ Utilities/Tests/FileUtility.Tests.ps1
@@ -1,9 +1,9 @@
 . ..\FileUtility\FileUtility.ps1
 
 $Global:configFilePath = "..\Metadata\Friends.spec.json"
-Describe "Basic File Checks" {
+Describe "FileUtility" {
 
-    Context "Test-EmptyFile function" {
+    Context "Test-EmptyFile" {
         #https://github.com/pester/Pester/wiki/TestDrive
         It "throws an exception if the file is empty" { 
             $emptyFile = "TestDrive:\empty.txt" # Join-Path "$TestDrive" "empty.txt"
@@ -13,7 +13,7 @@ Describe "Basic File Checks" {
                 Test-EmptyFile($emptyFile)
                 $ranOk = $true
             }
-            catch{}
+            catch {}
             $ranOk | Should be $false
         }
 
@@ -25,60 +25,62 @@ Describe "Basic File Checks" {
                 Test-EmptyFile($fileWithContent)
                 $ranOk = $true
             } 
-            catch{
+            catch {
                 $ranOk = $false
             }
             $ranOK | Should be $true
         }
     }
-}
 
-Describe "Config File Utility" {
-    It "fails when no config file is included" {
-        try {
-            $ranOk = $false
-            $jsonSpecFile = Get-FixedWidthJsonConfig 
-            $ranOk = $true
+
+
+    Context "Get-FixedWidthJsonConfig" {
+        It "fails when no config file is included" {
+            try {
+                $ranOk = $false
+                Get-FixedWidthJsonConfig 
+                $ranOk = $true
+            } 
+            catch {}
+            $ranOk | should be $false
+        }
+
+        It "succeeds when a config file is included" {
+            try {
+                $ranOk = $false
+                Get-FixedWidthJsonConfig $Global:configFilePath
+                $ranOk = $true
+            } 
+            catch {}
+            $ranOk | should be $true
+        }
+
+        It "returns a valid object when converting from Json" {
+            $configObject = Get-FixedWidthJsonConfig $Global:configFilePath
+            $fields = $configObject.Friends.Fields
+
+            $configObject.GetType() | should be "System.Management.Automation.PSCustomObject"
+            $fields.GetType() | should be "System.Object[]"
+
+            $configObject.Friends.Fields.Count | should be 5
+            $fields[0].datatype | should be "integer"
+            $fields[1].name | should be "FirstName"
+            $fields[2].length | should be "10"
+            $fields[4].pos | should be "59"
         } 
-        catch {}
-        $ranOk | should be $false
-    }
-
-    It "succeeds when a config file is included" {
-        try {
-            $ranOk = $false
-            $jsonSpecFile = Get-FixedWidthJsonConfig $Global:configFilePath
-            $ranOk = $true
-        } 
-        catch {}
-        $ranOk | should be $true
-    }
-
-    It "returns a valid object when converting from Json" {
-        $configObject = Get-FixedWidthJsonConfig $Global:configFilePath
-        $fields = $configObject.Friends.Fields
-
-        $configObject.GetType() | should be "System.Management.Automation.PSCustomObject"
-        $fields.GetType() | should be "System.Object[]"
-
-        $configObject.Friends.Fields.Count | should be 5
-        $fields[0].datatype | should be "integer"
-        $fields[1].name | should be "FirstName"
-        $fields[2].length | should be "10"
-        $fields[4].pos | should be "59"
-    } 
 
     
-}
+    }
 
-Describe "Data File Utility" {
-    It "gets data into an object" {
-        $csvPath = ".\data\SmallFile.csv"
-        $outputFWPath = ".\data\FWFile001.txt"
+    Context " Copy-CsvWithJsonConfigToFixedWidth" {
+        It "gets data into an object" {
+            $csvPath = ".\data\SmallFile.csv"
+            $outputFWPath = ".\data\FWFile001.txt"
 
-        Copy-CsvWithJsonConfigToFixedWidth $Global:configFilePath $csvPath $outputFWPath
-        $content = Get-Content $outputFWPath
-        $content | should be "asdfafd"
-        $content -match "a" | should be $true 
+            Copy-CsvWithJsonConfigToFixedWidth $Global:configFilePath $csvPath $outputFWPath
+            $content = Get-Content $outputFWPath
+            $content | should be "asdfafd"
+            $content -match "a" | should be $true 
+        }
     }
 }
