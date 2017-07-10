4b72487ebb2cd02888a6527ae6ad971fe0abb30a
Dennis Wells <dennis.circ@gmail.com>
2017-07-10 23:04:03 +0100

    Added utility to render to html
    thanks to the authors of ReportUnit
    https://www.nuget.org/packages/ReportUnit/

---
+++ Utilities/Tests/ReportUnit.ps1
@@ -0,0 +1,16 @@
+# Runs all test suites, passes the output to the NUnit format, renders it, displays in a browser
+# see my article here for installing nuget via Choco: https://dennisaa.wordpress.com/2017/04/26/powershell-updating-jpg-metadata/
+# https://www.nuget.org/packages/ReportUnit/
+nuget.exe install ReportUnit
+#Then, for example...
+# Execute all the tests
+$outputName = Get-Random
+$outputFile = "$PSScriptRoot/$outputName.xml"
+$htmlFile = "$PSScriptRoot/$outputName.html"
+Invoke-Pester -PassThru -Strict -OutputFile $outputFile -OutputFormat NUnitXml
+.\ReportUnit.1.2.1\tools\ReportUnit.exe $outputFile
+Start-Process chrome $htmlFile
+
+
+
+
