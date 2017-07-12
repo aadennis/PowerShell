cb91f6fb08a84e0bad074846adffa165a0e552d7
Dennis Wells <dennis.circ@gmail.com>
2017-07-12 05:35:50 +0100

    Add ignores, update ReportUnit comments

--- Utilities/Tests/ReportUnit.ps1
+++ Utilities/Tests/ReportUnit.ps1
@@ -1,5 +1,6 @@
 # Runs all test suites, passes the output to the NUnit format, renders it, displays in a browser
-# see my article here for installing nuget via Choco: https://dennisaa.wordpress.com/2017/04/26/powershell-updating-jpg-metadata/
+# see my article here for installing nuget via Choco: 
+# https://dennisaa.wordpress.com/2017/04/26/powershell-updating-jpg-metadata/
 # https://www.nuget.org/packages/ReportUnit/
 nuget.exe install ReportUnit
 #Then, for example...
@@ -10,7 +11,3 @@ $htmlFile = "$PSScriptRoot/$outputName.html"
 Invoke-Pester -PassThru -Strict -OutputFile $outputFile -OutputFormat NUnitXml
 .\ReportUnit.1.2.1\tools\ReportUnit.exe $outputFile
 Start-Process chrome $htmlFile
-
-
-
-
