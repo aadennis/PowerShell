04cd4a72b027464abf4673466c3699375a03ecdc
Dennis Wells <aadennis@users.noreply.github.com>
2017-07-15 02:10:52 +0100

    PowerShell mocks demo

---
+++ BoxOhTrix/MockDemo/Build.Tests.ps1
@@ -0,0 +1,31 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "BuildIfChanged" {
+    Context "When there are Changes" {
+    	Mock Get-Version {return 1.1}
+    	Mock Get-NextVersion {return 1.2}
+    	Mock Build {} -Verifiable -ParameterFilter {$version -eq 1.2}
+
+    	$result = BuildIfChanged
+
+	    It "Builds the next version" {
+	        Assert-VerifiableMocks
+	    }
+	    It "returns the next version number" {
+	        $result | Should Be 1.2
+	    }
+    }
+    Context "When there are no Changes" {
+    	Mock Get-Version { return 1.1 }
+    	Mock Get-NextVersion { return 1.1 }
+    	Mock Build {}
+
+    	$result = BuildIfChanged
+
+	    It "Should not build the next version" {
+	        Assert-MockCalled Build -Times 0 -ParameterFilter {$version -eq 1.1}
+	    }
+    }
+}
---
+++ BoxOhTrix/MockDemo/Build.ps1
@@ -0,0 +1,17 @@
+﻿function Build ($version) {
+    Write-Host "a build was run for version: $version"
+}
+
+function BuildIfChanged {
+	$thisVersion = Get-Version
+	$nextVersion = Get-NextVersion
+	if ($thisVersion -ne $nextVersion) { 
+        Build $nextVersion 
+    }
+	return $nextVersion
+}
+
+# The 
+function Get-Version {}
+
+function Get-NextVersion {}
