94beeb7f4dfd27f55f20d23fc86796173ca3a104
Dennis Wells <dennis.circ@gmail.com>
2017-07-15 02:31:15 +0100

    Tabs not spaces

--- BoxOhTrix/MockDemo/Build.Tests.ps1
+++ BoxOhTrix/MockDemo/Build.Tests.ps1
@@ -9,30 +9,30 @@ function Get-Version {}
 function Get-NextVersion {}
 
 Describe "BuildIfChanged" {
-    Context "When there are Changes" {
-    	Mock Get-Version {return 1.1}
-    	Mock Get-NextVersion {return 1.2}
-    	Mock Build {} -Verifiable -ParameterFilter {$version -eq 1.2}
+	Context "When there are Changes" {
+		Mock Get-Version {return 1.1}
+		Mock Get-NextVersion {return 1.2}
+		Mock Build {} -Verifiable -ParameterFilter {$version -eq 1.2}
 
-    	$result = BuildIfChanged
+		$result = BuildIfChanged
 
-	    It "Builds the next version" {
+		It "Builds the next version" {
 			Assert-VerifiableMocks
 			Assert-MockCalled Build -Times 1 -ParameterFilter {$version -eq 1.2}
-	    }
-	    It "returns the next version number" {
-	        $result | Should Be 1.2
-	    }
-    }
-    Context "When there are no Changes" {
-    	Mock Get-Version { return 1.1 }
-    	Mock Get-NextVersion { return 1.1 }
-    	Mock Build {}
+		}
+		It "returns the next version number" {
+			$result | Should Be 1.2
+		}
+	}
+	Context "When there are no Changes" {
+		Mock Get-Version { return 1.1 }
+		Mock Get-NextVersion { return 1.1 }
+		Mock Build {}
 
-    	BuildIfChanged
+		BuildIfChanged
 
-	    It "Should not build the next version" {
-	        Assert-MockCalled Build -Times 0 -ParameterFilter {$version -eq 1.1}
-	    }
-    }
+		It "Should not build the next version" {
+			Assert-MockCalled Build -Times 0 -ParameterFilter {$version -eq 1.1}
+		}
+	}
 }
--- BoxOhTrix/MockDemo/Build.ps1
+++ BoxOhTrix/MockDemo/Build.ps1
@@ -1,13 +1,13 @@
 ﻿function Build ($version) {
-    Write-Host "a build was run for version: $version"
+	Write-Host "a build was run for version: $version"
 }
 
 function BuildIfChanged {
 	$thisVersion = Get-Version
 	$nextVersion = Get-NextVersion
 	if ($thisVersion -ne $nextVersion) { 
-        Build $nextVersion 
-    }
+		Build $nextVersion 
+	}
 	return $nextVersion
 }
 
