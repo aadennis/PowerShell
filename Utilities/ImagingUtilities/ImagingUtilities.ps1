baef53bfc349cbe49eb3303ed602b2ea81168210
Dennis Wells <aadennis@users.noreply.github.com>
2017-08-25 23:30:21 +0100

    Conversion of files from e.g. pdf to png

---
+++ Utilities/ImagingUtilities/ImagingUtilities.ps1
@@ -0,0 +1,56 @@
+﻿
+# private function
+$gsDir = "C:\Program Files\gs\gs9.21\bin\" 
+
+#Quality of conversion an issue
+#https://stackoverflow.com/questions/5085710/how-to-improve-the-quality-of-jpeg-images-that-are-generated-from-pdfs-using-gho
+$argumentListStart = "-dNOPAUSE -dBATCH -sDEVICE=png16m -r600 -dDownScaleFactor=3 -sOutputFile="
+#$argumentListStart = "-dNOPAUSE -dBATCH -sDEVICE=jpeg -dJPEGQ=100 -sOutputFile="
+
+function Get-FileNameNoExtension ($fileName) {
+    $($fileName.Split("."))[0]
+}
+
+function ConvertTo-PngFromPdf {
+    Param (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+            $pdfFile = $(Throw "You must enter the name of the file to convert"),
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+            $folder = $(Throw "You must enter the name of the folder that contains the files")
+    ) 
+       $fileNameNoExtension = Get-FileNameNoExtension -fileName $pdfFile
+       $argumentList = $argumentListStart + "$folder/$fileNameNoExtension.jpg $folder/$fileNameNoExtension.pdf"
+       Start-Process -FilePath $gsDir/gswin64.exe -ArgumentList $argumentList
+    }
+    
+# end of private functions
+<#
+.SYNOPSIS
+Given a folder, convert all the PDF files in the folder to PNG format.
+From to a PNG file from a PDF file (assumes single page right now)
+
+.DESCRIPTION
+If a file is e.g. MyFile.pdf, then if MyFile.png already exists, then it will be overwritten.
+Dependency: GhostScript installed
+
+.EXAMPLE
+An example
+
+.NOTES
+#>
+#https://stackoverflow.com/questions/5085710/how-to-improve-the-quality-of-jpeg-images-that-are-generated-from-pdfs-using-gho
+function ConvertTo-PngSetFromPdf {
+Param (
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+        $PdfPngFolder = $(Throw "You must enter the name of the folder containing the files")
+) 
+    $pdfSet = Get-ChildItem -Path $PdfPngFolder -Filter *.pdf
+   foreach ($pdf in $pdfSet) {
+
+    Write-Host $pdf.FullName
+     ConvertTo-PngFromPdf -pdfFile $pdf.Name -folder $PdfPngFolder
+   }
+}
---
+++ Utilities/ImagingUtilities/UnitTests/ImagingUtilities.Tests.ps1
@@ -0,0 +1,25 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+
+# todo - store a test pdf in the test artifacts folder, and use that.
+
+. "$here\..\$sut"
+
+Describe "ConvertTo-PngFromPdf" {
+
+    It "Converts from a PDF to a PNG file" {
+        $folder = "c:/temp/x"
+        ConvertTo-PngSetFromPdf -PdfPngFolder $folder
+        $true | Should Be $false
+    }
+}
+
+#This to be moved to file utilities...
+Describe "Get-FileNameNoExtension" {
+    
+        It "Returns the name of a file without its extension" {
+            $file = "testingFile.xxxisx"
+            $fileNameNoExt = Get-FileNameNoExtension $file
+            $fileNameNoExt | Should Be "testingFile"
+        }
+    }
