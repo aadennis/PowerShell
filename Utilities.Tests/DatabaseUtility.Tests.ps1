7b05b64c7164fefede89d24af1c9d2a3b77458f0
Dennis Wells <dennis.circ@gmail.com>
2017-09-27 22:07:19 +0100

    Added config file to hold test arguments, refactoring

--- Utilities.Tests/DatabaseUtility.Tests.ps1
+++ Utilities.Tests/DatabaseUtility.Tests.ps1
@@ -3,12 +3,17 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $featureType = "Utilities"
 . "$here\..\$featureType\$sut"
 
-Describe "DatabaseUtilityx" {
+. "$here\Configuration\DbConfig.ps1"
 
-    # todo - store a test pdf in the test artifacts folder, and use that.
+Describe "DatabaseUtility" {
+    # don't want to hang around waiting for 30 seconds...
+    #https://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqlconnection.connectiontimeout(v=vs.110).aspx
+    $defaultDatabaseConnection = "Server=(localdb)\MSSQLLocalDB;Initial Catalog=tempdb;User Id = sa; Password={0};Connection Timeout=3;" -f $dbPassword
+
     Context "Open-DbConnection" {
-        It "opens the default database when no arguments are passed" {
-            Open-DbConnection
+        It "opens the default database (the items in the passed connection string must be right for the environment under test)" {
+            $dbConnection = Open-DbConnection -sqlConnectionString $defaultDatabaseConnection
+            $dbConnection.database | Should -Be "tempdb"
         }
     }
 }
