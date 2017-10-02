227c8597ad77453764ce9560acff2a8f194581ec
Dennis Wells <dennis.circ@gmail.com>
2017-10-02 21:04:06 +0100

    Tests for Format-Sql

--- Utilities.Tests/DatabaseUtility.Tests.ps1
+++ Utilities.Tests/DatabaseUtility.Tests.ps1
@@ -60,20 +60,38 @@ Describe "DatabaseUtility" {
             $dbConnection = Open-DbConnection -sqlConnectionString $defaultDatabaseConnectionString
             $workingDbName = "TestDb_{0}" -f $(Get-Random)
 
-            # act 1
             Execute-SQL $dbConnection $("create database {0}" -f $workingDbName)
-            Execute-SQL $dbConnection $("create table [{0}].[dbo].[mahTable] (id bigint identity, description nvarchar(80))" -f $workingDbName)
-            Execute-SQL $dbConnection $("insert into [{0}].[dbo].[mahTable] (description) values ('Twenty three pied pipers')" -f $workingDbName)
-            Execute-SQL $dbConnection $("insert into [{0}].[dbo].[mahTable] (description) values ('Twenty four pied pipers')" -f $workingDbName)
+            Execute-SQL $dbConnection $("create table [{0}].[dbo].[mahTable] (id bigint identity, description nvarchar(80), shoeSize int)" -f $workingDbName)
+            Execute-SQL $dbConnection $("insert into [{0}].[dbo].[mahTable] (description, shoeSize) values ('Twenty three pied pipers',8)" -f $workingDbName)
+            Execute-SQL $dbConnection $("insert into [{0}].[dbo].[mahTable] (description, shoeSize) values ('Twenty four pied pipers',12)" -f $workingDbName)
             
-             # act 2
-             $result = Read-SQL $dbConnection $("select * from  [{0}].[dbo].[mahTable]" -f $workingDbName)
-             $formattedResult = Format-sql $result
+            # test default call
+            # act 2
+            $result = Read-SQL $dbConnection $("select id, description, shoeSize from [{0}].[dbo].[mahTable] order by 1" -f $workingDbName)
+            $formattedResult = Format-sql $result
            
             # assert
             $formattedResult[0] | Should -Be "1^Twenty three pied pipers"
             $formattedResult[1] | Should -Be "2^Twenty four pied pipers"
 
+            # test reading of columns 1 to 3
+            # arrange
+            $expectedRows = @(
+                "1", "2",
+                "1^Twenty three pied pipers", "2^Twenty four pied pipers",
+                "1^Twenty three pied pipers^8", "2^Twenty four pied pipers^12"
+            )
+
+            # act / assert
+            $i = 0
+            1..3 | ForEach-Object {
+                $numberOfColumns = $_
+                $formattedResult = Format-Sql -dataset $result -columnCount $numberOfColumns
+                $formattedResult | % {
+                    $currentRow = $_
+                    $currentRow | Should be $expectedRows[$i++]
+                }
+            }
             Teardown-TestDatabase $dbConnection $workingDbName
         }
     }
