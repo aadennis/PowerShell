fc5d7b5c7567bc321b96ca806979fd974944cdcb
Dennis Wells <dennis.circ@gmail.com>
2017-10-03 23:30:09 +0100

    Added test [converts...
     from a delimited record to an object - non-default delimiter]

--- Utilities.Tests/ObjectUtility.Tests.ps1
+++ Utilities.Tests/ObjectUtility.Tests.ps1
@@ -14,5 +14,13 @@ Describe "ObjectUtility" {
             $result.field2 | Should -Be "99"
             $result.field3 | Should -Be "Orange-Peel"
         }
+        It "converts from a delimited record to an object - non-default delimiter" {
+            $csvRecord = "Apple!99!Orange-peel"
+            $csvHeader = "field1!Field2!fielD3"
+            $result = ConvertFrom-CsvRecordToObject -csvRecord $csvRecord -header $csvHeader -delimiter "!"
+            $result.field1 | Should -Be "Apple"
+            $result.field2 | Should -Be "99"
+            $result.field3 | Should -Be "Orange-Peel"
+        }
     }
 }
