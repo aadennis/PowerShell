4d3d943b8c979918ad7822aa9232886b81ec7b3d
Dennis Wells <aadennis@users.noreply.github.com>
2017-11-13 16:36:56 -0500

    Add files via upload

---
+++ Utilities/XmlParser/tester.ps1
@@ -0,0 +1,37 @@
+﻿
+function Show-OrderXml ($xmlFile) {
+    $xmlContent = [xml] $(Get-Content $xmlFile)
+
+    $purchaseOrder = $xmlContent.purchaseOrder
+    "purchaseOrder"
+    $purchaseOrder
+    
+    $shipTo = $purchaseOrder.shipTo
+    "shipTo"
+    $shipTo
+
+    $billTo = $purchaseOrder.billTo
+    $billTo
+
+    $comment = $purchaseOrder.comment
+    "comment"
+    $comment
+
+    $items = $purchaseOrder.items.ChildNodes
+    $items | % {
+        $item = $_
+        $item.partNum
+        $item.productName
+        $item.quantity
+        $item.USPrice
+        $item.shipDate
+        $item.comment
+    }
+
+    
+
+}
+
+Show-OrderXml -xmlFile "C:\temp\comp.xml"
+
+
