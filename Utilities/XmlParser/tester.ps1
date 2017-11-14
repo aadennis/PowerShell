e2ea63a44c553354666c8d5811f791b9299be93a
Dennis Wells <dennis.circ@gmail.com>
2017-11-14 08:45:07 +0000

    Colorize Write-Host

--- Utilities/XmlParser/tester.ps1
+++ Utilities/XmlParser/tester.ps1
@@ -1,25 +1,34 @@
-﻿
+﻿function Write-HostWithColor ($msg) {
+    Write-Host $msg -ForegroundColor Red -BackgroundColor Yellow
+}
+
 function Show-OrderXml ($xmlFile) {
     $xmlContent = [xml] $(Get-Content $xmlFile)
 
     $purchaseOrder = $xmlContent.purchaseOrder
-    "purchaseOrder"
+    Write-HostWithColor "purchaseOrder"
     $purchaseOrder
     
     $shipTo = $purchaseOrder.shipTo
-    "shipTo"
+    Write-HostWithColor "shipTo"
     $shipTo
 
     $billTo = $purchaseOrder.billTo
+    Write-HostWithColor "billTo"
     $billTo
 
     $comment = $purchaseOrder.comment
-    "comment"
+    Write-HostWithColor "comment"
     $comment
 
     $items = $purchaseOrder.items.ChildNodes
+    $itemsCount = $items.Count
+    Write-HostWithColor $itemsCount
+    $currentItemCount = 0
     $items | % {
         $item = $_
+        $currentItemCount++
+        Write-HostWithColor "\nPurchase Order Item [$currentItemCount]"
         $item.partNum
         $item.productName
         $item.quantity
@@ -27,11 +36,8 @@ function Show-OrderXml ($xmlFile) {
         $item.shipDate
         $item.comment
     }
-
-    
-
 }
 
-Show-OrderXml -xmlFile "C:\temp\comp.xml"
+Show-OrderXml -xmlFile "C:\Github\PowerShell\Utilities\XmlParser\comp.xml"
 
 
