7a1da00c73c052141074ac215a5ca4acbbfdc34a
Dennis Wells <aadennis@users.noreply.github.com>
2018-02-05 22:16:58 +0000

    Basic backup and restore in PowerShell SqlServer package

---
+++ SQLPS/SqlBackup.ps1
@@ -0,0 +1,42 @@
+﻿<#
+    This is just to illustrate backup in the SqlServer PowerShell package.
+    This also works against just LocalDB, although the warnings I see may be due to that.
+    SqlServer (note that SQLPS is now deprecated) - Backup databases (a sibling script restores).
+    This is just to show the very basics of Backup-SqlDatabase.
+    It will backup any user databases it finds for the given instance, saving the files to a naive (in that it may not
+    be unique) temporary location.
+    Only ever run this script as-is on a play server
+
+    Dependencies for executing the SqlServer package:
+      Install-PackageProvider -Name NuGet
+      Install-Module -Name SqlServer -AllowClobber
+
+    If credentials are required, then add this kind of thing...
+    if ($creds -eq $null) {
+        $creds = get-credentials
+    }
+    Then plug $creds into the -credential switch in backup-database and restore-database
+
+#>
+# dodgy, naive...
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+Import-Module SqlServer
+
+$sqlInstance = "`(Localdb`)\MSSQLLocalDB"
+# naive...
+$tempDirName = "c:/temp/DbBackup" + $(Get-Random(1..1000))
+
+"Temporary location of backup files is [$tempDirName]"
+New-Item -Path $tempDirName -ItemType Directory
+
+# This gets all user databases...
+$dbSet = gci SQLSERVER:\SQL\$sqlInstance\Databases
+$dbset | % {
+    $currentDatabase = $_.name
+    "Backing up... [$currentDatabase]"
+    $backupFile = $tempDirName + "/${currentDatabase}"
+    $backupFile
+    Backup-SqlDatabase -ServerInstance $sqlInstance -Database $currentDatabase -BackupFile $backupFile 
+}
+
---
+++ SQLPS/SqlRestore.ps1
@@ -0,0 +1,38 @@
+﻿<#
+    This is just to illustrate backup and restore in the SqlServer PowerShell package.
+    This also works against just LocalDB, although the warnings I see may be due to that.
+    SqlServer (note that SQLPS is now deprecated) - Restore databases.
+    This is just to show the very basics of Backup-SqlDatabase and Restore-SqlDatabase.
+    It will restore any user databases it finds in the given set of database files.
+    Only ever run this script as-is on a play server
+
+    Dependencies for executing the SqlServer package:
+      Install-PackageProvider -Name NuGet
+      Install-Module -Name SqlServer -AllowClobber
+
+    If credentials are required, then add this kind of thing...
+    if ($creds -eq $null) {
+        $creds = get-credentials
+    }
+    Then plug $creds into the -credential switch in backup-database and restore-database
+
+#>
+# This changes depending on the location of your backups, der...
+$backupLocation = "c:/temp/DbBackup434"
+
+# dodgy, naive...
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Import-Module SqlServer
+$sqlInstance = "`(Localdb`)\MSSQLLocalDB"
+
+"Location of backup files is [$backupLocation]"
+
+# Principle now is that you don't know or care what name the databases have - that is carried in metadata and the file name...
+cd $backupLocation
+gci | % {
+    $currentDatabase = $_.name
+    "Restoring... [$currentDatabase]"
+    $backupFile = $backupLocation + "/${currentDatabase}"
+    $backupFile
+    Restore-SqlDatabase -ServerInstance $sqlInstance -Database $currentDatabase -BackupFile $backupFile
+}
