21af5c600832b0672b98cab91c6a03187af7cae0
Dennis Wells <aadennis@users.noreply.github.com>
2018-07-07 12:23:51 +0100

    sandbox - returning an array

---
+++ BoxOhTrix/CallerStuff/ArrayPlayCallee.ps1
@@ -0,0 +1,10 @@
+function bb() {
+    $x = [System.Collections.ArrayList] @()
+    $x += "row 1"
+    $x += "row 2"
+    $x += "row 3"
+    $x += "row 4"
+    $x
+}
+
+
---
+++ BoxOhTrix/CallerStuff/ArrayPlayCaller.ps1
@@ -0,0 +1,15 @@
+function aa() {
+    bb
+}
+
+. ./ArrayPlayCallee.ps1
+
+$a = aa
+
+$a.Count
+$a[3]
+
+
+
+
+
