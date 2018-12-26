3b57014c2567b1a0cde2ae5dd6f31def7f2012b4
Dennis Wells <dennis.circ@gmail.com>
2018-12-26 18:07:05 +0000

    validation, metadata
    Add validation, remove example metadata as now in separate file as example.
    Validation: see comment ref Performance counter .Net static method

--- PerformanceCounters/Get-SqlPerfCounters.ps1
+++ PerformanceCounters/Get-SqlPerfCounters.ps1
@@ -14,11 +14,51 @@ Get-Counter -Counter "\\desktop-3fe014p\physicaldisk(_total)\avg. disk writes/se
 
 Note that $ in SQL etc must be escaped (e.g. $SQLEXPRESS)
 #>
+function Show-Error {
+    param (
+      $message
+    )
+    Write-Host $message -ForegroundColor Yellow -BackgroundColor Red
+}
+
+function Show-Status {
+    param (
+      $message
+    )
+    Write-Host $message -ForegroundColor Yellow -BackgroundColor DarkBlue
+}
+function Check-CounterExists {
+    "Checking that requested counter exist..."
+    # $aa = [System.Diagnostics.PerformanceCounterCategory]::GetCategories(();
+    # https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancecountercategory.counterexists?view=netframework-4.7.2
+    # However, seem to need to test for both category and counter, as category can contain nonsense and still return true for
+    # for ::Exists($category).
+    # ignore until I understand why e.g. _total is not valid
+    
+    $counters | % {
+        $currentCounter = $_
+        $a = $currentCounter.ToString().Split("\")
+        $category = $a[-2]
+        $counter = $a[-1]
+        $catExists = [System.Diagnostics.PerformanceCounterCategory]::Exists($category)
+        if (!$catExists) {
+            Show-Error "Bad category: $currentCounter" 
+            break
+        }
+        $counterExists = [System.Diagnostics.PerformanceCounterCategory]::CounterExists($counter,$category)
+        if (!$counterExists) {
+            Show-Error "Bad counter: $currentCounter" 
+            break
+        }
+    }
+}
+
+# entry point
 
 $counterMetadataPath = "C:\GitHub\PowerShell\PerformanceCounters\Metadata\PerformanceCountersToRecord.txt"
 $sut = "\\" + $env:COMPUTERNAME #'\\desktop-3fe014p'
 $sqlInstance = "MSSQL`$SQLEXPRESS"
-$maxSamples = 10
+$maxSamples = 5
 $outputFile = "c:\temp\PerfCountersOutput.csv"
 $commentCharacter = "#"
 
@@ -31,8 +71,11 @@ Get-Content -Path $counterMetadataPath | % {
     }
 }
 
-"Running Performance counters. Finishes in about [${maxSamples}] seconds"
-"Results will be in [$outputFile]"
+# ignore - see function comments
+# Check-CounterExists
+
+Show-Status "Running Performance counters. Finishes in about [${maxSamples}] seconds"
+Show-Status "Results will be in [$outputFile]"
 
 $results = Get-Counter -Counter $counters -MaxSamples $maxSamples | ForEach {
     $_.CounterSamples | ForEach {
@@ -45,22 +88,3 @@ $results = Get-Counter -Counter $counters -MaxSamples $maxSamples | ForEach {
 } 
 $results | Out-GridView
 $results | Export-Csv -Path $outputFile -NoTypeInformation
-
-<#
-Example content of PerfCountersToRecord.txt:
-
-memory\% committed bytes in use
-memory\cache faults/sec
-physicaldisk(_total)\% disk time
-physicaldisk(_total)\current disk queue length
-system\Processor Queue Length
-memory\available mbytes
-physicaldisk(_total)\avg. disk sec/read
-physicaldisk(_total)\avg. disk sec/write
-physicaldisk(_total)\disk reads/sec
-physicaldisk(_total)\disk writes/sec
-processor(_total)\% processor time
-${sqlInstance}:General Statistics\User Connections
-${sqlInstance}:Memory Manager\Memory Grants Pending
-${sqlInstance}:SQL Statistics\Batch Requests/sec
-#>
