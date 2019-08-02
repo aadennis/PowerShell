fcfb68f2fce1783f67fd23695f297985b61a5fa4
Dennis Wells <dennis.circ@gmail.com>
2019-08-02 23:25:53 +0100

    Test and function to return files in a folder

--- Utilities.Tests/DirectoryUtility.Tests.ps1
+++ Utilities.Tests/DirectoryUtility.Tests.ps1
@@ -7,10 +7,22 @@ $Global:configFilePath = "$here\Metadata\Friends.spec.json"
 Describe "DirectoryUtility" {
 
     Context "Get-FileNamesInFolder" {
-        It "Returns (right now) the folder name it was passed" {
-            $folder = "testingFolder"
-            $fileNameNoExt = Get-FileNamesInFolder $folder
-            $fileNameNoExt | Should Be $folder
+        It "Returns the names of the given folder, sorted by name" {
+            # Sadly, Directory class does not play ball with TestDrive
+            #New-Item "TestDrive:\Testfolder" -ItemType Dir
+            $testFolder = New-Item -Path "C:\temp\$(Get-Random)" -ItemType Dir
+            $testFileSet = "File123.txt", "File789.txt", "aardvark","File456.txt"
+            $testFileSet | % {
+                New-Item -Path "$testFolder\$_" -ItemType File
+            }
+
+            $fileNameSet = Get-FileNamesInFolder $testFolder
+            $fileNameSet.length | Should Be 4
+
+            $fileNameSet[0] | Should Be $(Join-Path $testFolder $testFileSet[2])
+            $fileNameSet[1] | Should Be $(Join-Path $testFolder $testFileSet[0])
+            $fileNameSet[2] | Should Be $(Join-Path $testFolder $testFileSet[3])
+            $fileNameSet[3] | Should Be $(Join-Path $testFolder $testFileSet[1])
         }
     }
 }
--- Utilities/DirectoryUtility.ps1
+++ Utilities/DirectoryUtility.ps1
@@ -1,9 +1,13 @@
 # mostly uses .Net System.IO static classes Directory and DirectoryInfo
+# https://docs.microsoft.com/en-us/dotnet/api/system.io.directory?view=netframework-4.8
+# https://docs.microsoft.com/en-us/dotnet/api/system.io.directoryinfo?view=netframework-4.8
 Set-StrictMode -Version latest
 
 <#
 .SYNOPSIS
-    Get names for all files inside a single folder - not recursive
+    Get names for all files inside a single folder - not recursive.
+    Note the EnumerateFiles method returns the array sorted by name
+
 .DESCRIPTION
     Long description
 .EXAMPLE
@@ -22,5 +26,6 @@ function Get-FileNamesInFolder {
         [Parameter(Mandatory=$true)]
         $Folder
     )
-    $Folder
+    #[System.IO.Directory]::EnumerateFiles($Folder, "*.txt")
+    [System.IO.Directory]::EnumerateFiles($Folder)
 }
