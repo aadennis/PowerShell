352f99998e4fabf26d4dcb374d4b898a132ae41d
Dennis Wells <dennis.circ@gmail.com>
2019-08-03 10:32:09 +0100

    You can use TestDrive and .Net

--- Utilities.Tests/DirectoryUtility.Tests.ps1
+++ Utilities.Tests/DirectoryUtility.Tests.ps1
@@ -9,19 +9,21 @@ function  Remove-FolderSilent ($folderPath) {
     Remove-Item -Path $folderPath -Force -Recurse -ErrorAction SilentlyContinue
 }
 
+function New-TempFolder {
+    #TestDrive and .Net - https://github.com/pester/Pester/wiki/TestDrive#working-with-net-objects
+    New-Item -Path "$TestDrive\$(Get-Random)" -ItemType Dir
+}
+
 function TearDown($folderPath) {
     Remove-FolderSilent -folderPath $folderPath
 }
 
 Describe "DirectoryUtility" {
-
     Context "Get-FileNamesInFolder" {
         It "Returns the pathnames of the files in the given folder, sorted by name" {
-            # Sadly, Directory class does not play ball with TestDrive
-            #New-Item "TestDrive:\Testfolder" -ItemType Dir
 
             # arrange 
-            $testFolder = New-Item -Path "C:\temp\$(Get-Random)" -ItemType Dir
+            $testFolder = New-TempFolder
             $testFileSet = "File123.txt", "File789.txt", "aardvark","File456.txt"
             $testFileSet | ForEach-Object {
                 New-Item -Path "$testFolder\$_" -ItemType File
@@ -36,16 +38,13 @@ Describe "DirectoryUtility" {
             $fileNameSet[1] | Should Be $(Join-Path $testFolder $testFileSet[0])
             $fileNameSet[2] | Should Be $(Join-Path $testFolder $testFileSet[3])
             $fileNameSet[3] | Should Be $(Join-Path $testFolder $testFileSet[1])
-
-            TearDown -folderPath $testFolder
         }
     }
 
     Context "Get-LowNameInFolder" {
         It "Returns the lowest-named pathname (e.g folder\aardvark) in the given folder" {
-            #Start-Sleep 10
               # arrange 
-              $testFolder = New-Item -Path "C:\temp\$(Get-Random)" -ItemType Dir
+              $testFolder = New-TempFolder
               $testFileSet = "File123.txt", "File789.txt", "aardvark","File456.txt"
               $testFileSet | ForEach-Object {
                   New-Item -Path "$testFolder\$_" -ItemType File
@@ -56,15 +55,13 @@ Describe "DirectoryUtility" {
   
               # assert
               $file | Should Be $(Join-Path $testFolder "aardvark")
-
-              TearDown -folderPath $testFolder
         }
     }
 
     Context "Get-HighNameInFolder" {
         It "Returns the highest-named pathname (e.g folder\zygote) in the given folder" {
                 #arrange 
-                $testFolder = New-Item -Path "C:\temp\$(Get-Random)" -ItemType Dir
+                $testFolder = New-TempFolder
                 $testFileSet = "File123.txt", "File789.txt", "zygote", "aardvark","File456.txt"
                 $testFileSet | ForEach-Object {
                     New-Item -Path "$testFolder\$_" -ItemType File
@@ -75,8 +72,6 @@ Describe "DirectoryUtility" {
     
                 # assert
                 $file | Should Be $(Join-Path $testFolder "zygote")
-
-                TearDown -folderPath $testFolder
         }
     }
 }
