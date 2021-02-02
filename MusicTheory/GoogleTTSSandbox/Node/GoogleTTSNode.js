e598e1dbb61245a59e8bc91d76f77acbcfbb14a1
Happy Toons <happytoons141@ymail.com>
2021-02-02 19:41:30 +0000

    Parameterized: languageCode and SsmlGender can be passed, else defaults apply.

--- MusicTheory/GoogleTTSSandbox/Node/GoogleTTSNode.js
+++ MusicTheory/GoogleTTSSandbox/Node/GoogleTTSNode.js
@@ -4,15 +4,36 @@ const fs = require('fs');
 const util = require('util');
 
 const client = new textToSpeech.TextToSpeechClient();
+let inputFile, outputFile, inputlanguageCode = 'en-GB', inputSsmlGender = 'FEMALE';
 
-const outputFile = 'c:/temp/nodee.mp3';
-const inputFile = 'c:/temp/inner.txt'; // text file - no quotes at start and end, a CR should be the final character.
-const readFile = util.promisify(fs.readFileSync);
+// see HappyToons repo
+const args = process.argv
+args.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+    switch (index) {
+        case 2:
+            // text file - no quotes at start and end, a CR should be the final character.
+            inputFile = val;
+            break;
+        case 3:
+            outputFile = val;
+            break;
+        case 4:
+            inputlanguageCode = val;
+            break;
+        case 5:
+            inputSsmlGender = val;
+            break;
+        default:
+    }
+});
+
 const textToSpeak = fs.readFileSync(inputFile);
 
 const request = {
     input: { text: textToSpeak },
-    voice: { languageCode: 'en-GB', ssmlGender: 'FEMALE' },
+    voice: { languageCode: inputlanguageCode, ssmlGender: inputSsmlGender },
     audioConfig: { audioEncoding: 'MP3' },
 };
 
@@ -25,3 +46,5 @@ async function run() {
 
 run();
 
+// Example call
+// node .\GoogleTTSNode.js 'c:/temp/inner.txt' 'c:/temp/speech.mp3'
